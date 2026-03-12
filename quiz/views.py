e31748179be2b5c671433e471c09e6d20428f598
Arthur-wz <tukomedina0518@gmail.com>
2026-03-12 11:46:14 -0300

    coisas

--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
+from .models import Pergunta
+import random
 
 def home(request):
     return render(request, "home.html")
+
+def jogo(request):
+
+    perguntas = list(Pergunta.objects.all())
+
+    pergunta = random.choice(perguntas)
+
+    return render(request, "jogo.html", {"pergunta": pergunta})
